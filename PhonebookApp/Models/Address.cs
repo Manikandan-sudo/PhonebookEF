4cae878a0a2d01f2cbab09fcecdcffaa48e9750e
Manikandan-sudo <manikandanmurugesancse@gmail.com>
2020-07-24 09:54:42 +0530

    Local-Branch

--- PhonebookApp/Models/Address.cs
+++ PhonebookApp/Models/Address.cs
@@ -12,15 +12,19 @@ namespace PhonebookApp.Models
     using System;
     using System.Collections.Generic;
     
-    public partial class Person
+    public partial class Address
     {
         public int ID { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string PhoneNumber { get; set; }
-        public bool IsActive { get; set; }
-        public string EmailAddress { get; set; }
+        public string AddressOne { get; set; }
+        public string AddressTwo { get; set; }
+        public int PinCode { get; set; }
+        public int CountryName { get; set; }
+        public int StateName { get; set; }
+        public int CityName { get; set; }
     
-        public virtual Address Address { get; set; }
+        public virtual City City { get; set; }
+        public virtual Country Country { get; set; }
+        public virtual Person Person { get; set; }
+        public virtual State State { get; set; }
     }
 }
